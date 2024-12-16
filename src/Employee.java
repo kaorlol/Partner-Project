241cc4d96118c8afeba87abc5ac1a64b539353ed
Jacoby Blanchette <jacobyb0508@gmail.com>
2024-12-16 09:54:27 -0500

    Semi added jobpay

--- src/Employee.java
+++ src/Employee.java
@@ -6,28 +6,43 @@ public class Employee {
 	private double MinimumTipWage = 7.33;
 	private double OvertimeRate = 1.5;
 
+	private class JobPay {
+		public Job job;
+		public double pay;
+
+		public JobPay(Job j, double p) {
+			job = j;
+			pay = p;
+		}
+	}
+
+	private enum Job {
+		Manager,
+		Cook,
+		Cashier
+	}
+
 	Dictionary<String, Double> EmployeeDatabase = new Hashtable<>();
 
-	public String Manager = "";
-	public String Cook = "";
-	public String Cashier = "";
+	// public String Manager = "";
+	// public String Cook = "";
+	// public String Cashier = "";
 
 	public Employee(String name, String job, double pay) {
 		switch (job) {
 			case "manager":
-				Manager = name;
+				// EmployeeDatabase.put(name, new JobPay(job, pay))
 				break;
 			case "cook":
-				Cook = name;
+	
 				break;
 			case "cashier":
-				Cashier = name;
 			default:
 				break;
 		}
 	}
 
 	public Employee(String name, String job) {
-		
+
 	}
 }
