d4f03c279df47e6c94a06360f21e570942066025
Jacoby Blanchette <jacobyb0508@gmail.com>
2024-12-16 08:55:12 -0500

    Init

---
+++ src/Employee.java
@@ -0,0 +1,17 @@
+public class Employee {
+	private double MinimumWage = 14.65;
+	private double MinimumTipWage = 7.33;
+	private double OvertimeRate = 1.5;
+
+	public String Cook = "";
+	public String Manager = "";
+	public String Cashier = "";
+
+	public Employee(String name, String job, double pay) {
+
+	}
+
+	public Employee(String name, String job) {
+		
+	}
+}
